05751e08f847687f80b66ffc134f2c2d68ec8e7b
Even Marius Nordhagen <evenmn@student.matnat.uio.no>
2017-10-10 10:03:01 -0700

    Energy calculated, but is not fully correct

--- FYSKJM4480/Project1/project1.py
+++ FYSKJM4480/Project1/project1.py
@@ -50,18 +50,34 @@ def call_psi4(mol_spec, extra_opts = {}):
     return SCF_E_psi4, Enuc, H,W,S, nbf, nalpha, nbeta
 
 
-def density(U, N):
+# === UHF ===
+
+def density_matrix(U, N, noise=False):
     '''
     Density matrix where U is 
     occupied and N is the number of 
     occupied states
     '''
     U = np.matrix(U[:,:N])
-    D = np.dot(U, U.H)
+    D = np.dot(U, U.H)      # D is a symmetric matrix
+    if noise:
+        X = np.random.rand(len(U), len(U[0]))
+        X = X + np.transpose(X)
+        D += 0.001*X
     return D
+
+
+def J_matrix(D, W):
+    '''J(D)_pq = sum_rs (pq|rs) D_sr     Using Mulliken notation'''
+    return np.einsum('pqrs, sr->pq', W, D)
+
+
+def K_matrix(D, W):
+    '''K(D)_pq = sum_rs (ps|rq) D_sr     Using Mulliken notation'''
+    return np.einsum('psrq, sr->pq', W, D)
     
     
-def fock_operator(H, W, D_up, D_dn):
+def uhf_fock_matrices(H, W, D_up, D_dn):
     '''
     Constructing the fock operators
     F_up = h + J(D_up + D_dn) - K(D_up),
@@ -70,33 +86,81 @@ def fock_operator(H, W, D_up, D_dn):
     W, and the density matrices D_up
     and D_down
     '''
-    # J(D)_pq = sum_rs (pq|rs) D_sr     Using Mulliken notation
-    J = np.einsum('pqrs, sr->pq', W, D_up + D_dn)
-    
-    # K(D)_pq = sum_rs (ps|rq) D_sr     Using Mulliken notation
-    K_up = np.einsum('psrq, sr->pq', W, D_up)
-    K_dn = np.einsum('psrq, sr->pq', W, D_dn)
+    J    = J_matrix(D_up + D_dn, W)
+    K_up = K_matrix(D_up, W)
+    K_dn = K_matrix(D_dn, W)
     
     F_up = H + J + K_up
     F_dn = H + J + K_dn
     return F_up, F_dn
     
     
-def UHF_solver(mol_geo):
+def UHF_SCF_solver(mol_geo, plot=False):
     SCF_E_psi4, Enuc, H, W, S, nbf, n_up, n_dn = \
     call_psi4(mol_geo, extra_opts = {}) 
     
     # Need to find U by solving the generalized eigenvalue problem He=uSe
     e, u = eigh(H, S)
-    D_up = density(u, n_up)
-    D_dn = density(u, n_dn)
-    F_up, F_dn = fock_operator(H, W, D_up, D_dn)
+    D_up = density_matrix(u, n_up)
+    D_dn = density_matrix(u, n_dn)
+    F_up, F_dn = uhf_fock_matrices(H, W, D_up, D_dn)
 
     # Again we have generalized eigenvalue problem 
-    epsilon_up, u_up = eigh(F_up, S)
-    epsilon_dn, u_dn = eigh(F_dn, S)
-    return SCF_E_psi4, epsilon_up, epsilon_dn
+    epsilon_up = eigh(F_up, S, eigvals_only=True)
+    epsilon_dn = eigh(F_dn, S, eigvals_only=True)
+    
+    # Energy calculations 
+    print('Calculated UHF_energy:   ', UHF_energy(D_up, D_dn, H, W))
+    print('Exact UHF_energy (Psi4): ', SCF_E_psi4)
+    
+    # Plots the eigenvalues as a energy spectrum
+    if plot:
+        plot_energy_spectrum(epsilon_up, epsilon_dn)
+    
+    return None
 
+
+def UHF_energy(D_up, D_dn, H, W):
+    '''
+    Use theory from section 2.1
+    E_UHF = Fock_energy (E_F) + Hartree_energy (E_H) - Exchange_energy (E_EX)
+    J(D)_{pq} = sum_{rs}(pq|rs)D_{sr}
+    K(D)_{pq} = sum_{rs}(ps|rq)D_{sr}
+    '''
+    J_up = J_matrix(D_up, W)
+    J_dn = J_matrix(D_dn, W)
+    K_up = K_matrix(D_up, W)
+    K_dn = K_matrix(D_dn, W)
+    
+    # F^s = h + J(D_up + D_dn) - K(D^s) where s is the spin 
+    F_up = H + J_matrix(D_up + D_dn, W) - K_up
+    F_dn = H + J_matrix(D_up + D_dn, W) - K_dn
+    
+    # E_F = sum_{i,s}(phi_i^s|h|phi_i^s)
+    E_F =   np.einsum('pq, qp->', F_up, D_up) \
+          + np.einsum('pq, qp->', F_dn, D_dn)
+    
+    # E_H = 1/2 sum (ii|jj) - Sum over all possible combinations of J^s, D^s
+    E_H =  0.5*(np.einsum('pq, qp->', J_up, D_up)\
+              + np.einsum('pq, qp->', J_up, D_dn)\
+              + np.einsum('pq, qp->', J_dn, D_up)\
+              + np.einsum('pq, qp->', J_dn, D_dn))
+
+    # E_H = 1/2 sum (ii|jj) - Sum over all possible combinations of J^s, D^s
+    E_EX = 0.5*(np.einsum('pq, qp->', K_up, D_up)\
+              + np.einsum('pq, qp->', K_dn, D_dn))
+              
+    return E_F + E_H - E_EX
+    
+    
+def plot_energy_spectrum(e_up, e_dn):
+    '''Plots a horizontal energy spectrum for mol_geo'''
+    for e in e_up:
+        plt.axhline(e, linewidth=0.5)
+    plt.title('Energy spectrum for $H_2O$')
+    plt.ylabel('Energy, [Hartree]')
+    plt.show()
+    
 if __name__ == '__main__':
     pass
 
@@ -123,5 +187,4 @@ h2 = """
     units bohr
 """ .format(r_h2)
 
-psi4_energy, e_up, e_dn = UHF_solver(h2o)
-print(psi4_energy)
+UHF_SCF_solver(h2o, plot=True)
