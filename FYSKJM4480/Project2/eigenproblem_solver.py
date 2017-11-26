293aac906243a6d1b18c1fe177893993fe565dbe
evenmn <evenmn@student.matnat.uio.no>
2017-11-26 15:33:08 -0800

    Updated some stuff

--- FYSKJM4480/Project2/eigenproblem_solver.py
+++ FYSKJM4480/Project2/eigenproblem_solver.py
@@ -9,20 +9,24 @@ eigenvalues_FCI = []                    # Going to be a nested list (matrix)
                                         # with eigenvalues for each g
 
 for g in g_list:
-    M_FCI = np.matrix([[-2 - g, -0.5 * g, -0.5 * g, -0.5 * g, -0.5 * g, 0], \
-                       [-0.5 * g, -4 - g, -0.5 * g, -0.5 * g, 0, -0.5 * g], \
-                       [-0.5 * g, -0.5 * g, -6 - g, 0, -0.5 * g, -0.5 * g], \
-                       [-0.5 * g, -0.5 * g, 0, -6 - g, -0.5 * g, -0.5 * g], \
-                       [-0.5 * g, 0, -0.5 * g, -0.5 * g, -8 - g, -0.5 * g], \
-                       [0, -0.5 * g, -0.5 * g, -0.5 * g, -0.5 * g, -10 - g]])
+    M_FCI = np.matrix([[2 - g, -0.5 * g, -0.5 * g, -0.5 * g, -0.5 * g, 0], \
+                       [-0.5 * g, 4 - g, -0.5 * g, -0.5 * g, 0, -0.5 * g], \
+                       [-0.5 * g, -0.5 * g, 6 - g, 0, -0.5 * g, -0.5 * g], \
+                       [-0.5 * g, -0.5 * g, 0, 6 - g, -0.5 * g, -0.5 * g], \
+                       [-0.5 * g, 0, -0.5 * g, -0.5 * g, 8 - g, -0.5 * g], \
+                       [0, -0.5 * g, -0.5 * g, -0.5 * g, -0.5 * g, 10 - g]])
                        
     eigenvalues_FCI.append((np.linalg.eigh(M_FCI)[0]))
 
-for k in range(M_FCI.shape[0]):         # M_FCI.shape[0] is length(col1(M_FCI))
+for k in reversed(range(M_FCI.shape[0])):         # M_FCI.shape[0] is length(col1(M_FCI))
     new_list_FCI = []
     for i in range(len(eigenvalues_FCI)):
         new_list_FCI.append(eigenvalues_FCI[i][k])
-    plt.plot(g_list, new_list_FCI, label='Eigenvalue {}'.format(k))
+    if k == 0:
+        lw = 3
+    else:
+        lw = 1
+    plt.plot(g_list, new_list_FCI, label='k={}'.format(k), linewidth=lw)
 plt.legend(loc='best')
 plt.title('Eigenvalues')
 plt.xlabel('g')
@@ -35,15 +39,19 @@ eigenvalues_CID = []                        # Going to be a nested list (matrix)
                                         # with eigenvalues for each g
 
 for g in g_list:
-    M_CID = np.matrix([[-2-g, 0, 0], [0, -4-g, -0.5*g], [0, -0.5*g, -10-g]])
+    M_CID = np.matrix([[2 - g, -0.5 * g, -0.5 * g, -0.5 * g, -0.5 * g], \
+                       [-0.5 * g, 4 - g, -0.5 * g, -0.5 * g, 0], \
+                       [-0.5 * g, -0.5 * g, 6 - g, 0, -0.5 * g], \
+                       [-0.5 * g, -0.5 * g, 0, 6 - g, -0.5 * g], \
+                       [-0.5 * g, 0, -0.5 * g, -0.5 * g, 8 - g]])
                        
     eigenvalues_CID.append((np.linalg.eigh(M_CID)[0]))
 
-for k in range(M_CID.shape[0]):         # M_CID.shape[0] is length(col1(M_CID))
+for k in reversed(range(M_CID.shape[0])):         # M_CID.shape[0] is length(col1(M_CID))
     new_list_CID = []
     for i in range(len(eigenvalues_CID)):
         new_list_CID.append(eigenvalues_CID[i][k])
-    plt.plot(g_list, new_list_CID, label='Eigenvalue {}'.format(k))
+    plt.plot(g_list, new_list_CID, label='k={}'.format(k))
 plt.legend(loc='best')
 plt.title('Eigenvalues')
 plt.xlabel('g')
@@ -52,7 +60,8 @@ plt.savefig('eigenvalues_CID.png')
 plt.show()
 
 # --- Comparison of ground-state energy ---
-
+E = lambda g: 1 - g**2 + (25/48)*g**2
+plt.plot(g_list, E(g_list))
 plt.plot(g_list, new_list_FCI, label='FCI GS')
 plt.plot(g_list, new_list_CID, label='CID GS')
 plt.title('FCI and CID ground-state comparison')
