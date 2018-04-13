4d5b4bd260d01728f60d6a4d85882337980b1145
Even <evenmn@fys.uio.no>
2018-04-13 17:46:27 +0200

    random_walkers.py added

---
+++ FYS4130/random_walkers.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+T  = 1000
+dt = 0.1
+dx = 0.1
+N  = int(T/dt)
+
+move = np.random.random(N)
+move_disc = np.where(move>0.5,1,-1)
+
+x = np.zeros(N)
+sumu = 0
+for i in range(N):
+    sumu += move_disc[i]
+    x[i] = sumu
+    
+t = np.linspace(0,10,N)
+plt.plot(x, t)
+plt.show()
+
